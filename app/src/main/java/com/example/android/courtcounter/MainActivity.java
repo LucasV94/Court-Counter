3be1cff950967c22c7de3779f90b3982992f3ef4
Lucasv94 <lucasvasquez94@gmail.com>
2016-04-07 15:55:09 -0700

    Added Basic Button function for team A

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    //There has to be a better way to do this
+    public void threePointToA(View view) {
+        displayForTeamA(3);
+    }
+
+    public void twoPointToA(View view) {
+        displayForTeamA(2);
+    }
+
+    public void freeThrowToA(View view) {
+        displayForTeamA(1);
+    }
+
+
 }
+
